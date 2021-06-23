64b849294ce4a4b1f93c8af4300cb6d8514a11a6
ganandak6 <42195872+ganandak6@users.noreply.github.com>
2021-06-23 06:51:06 -0500

    ML.NET-Binary Classification for Hearth Disease
    ML.NET-Binary Classification for Hearth Disease

---
+++ ML-Heart-Desease/HeartDesease.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML_Heart_Desease
+{
+    public class HeartDesease
+    {
+        [LoadColumn(0)]
+        public float age { get; set; }
+        [LoadColumn(1)]
+        public float sex { get; set; }
+        [LoadColumn(2)]
+        public float cp { get; set; }
+        [LoadColumn(3)]
+        public float trestnps { get; set; }
+        [LoadColumn(4)]
+        public float chol { get; set; }
+        [LoadColumn(5)]
+        public float fbs { get; set; }
+        [LoadColumn(6)]
+        public float restecg { get; set; }
+        [LoadColumn(7)]
+        public float thalach { get; set; }
+        [LoadColumn(8)]
+        public float exang { get; set; }
+        [LoadColumn(9)]
+        public float oldpeak { get; set; }
+        [LoadColumn(10)]
+        public float slope { get; set; }
+        [LoadColumn(11)]
+        public float ca { get; set; }
+        [LoadColumn(12)]
+        public float thal { get; set; }
+        [LoadColumn(13)]
+        [ColumnName("Label")]
+        //[Column(Ordinal: "13", name: "Label")]
+        public bool target { get; set; }
+        // public float Features { get; set; }
+    }
+
+    /*
+     private class RegressionData
+{
+    [LoadColumn(0, 10), ColumnName("Features")]
+    public float FeatureVector { get; set;}
+
+    [LoadColumn(11)]
+    public float Target { get; set;}
+}*/
+
+    public class HeartDeseaseOutput : HeartDesease
+    {
+        // ColumnName attribute is used to change the column name from
+        // its default value, which is the name of the field.
+        [ColumnName("PredictedLabel")]
+        public String Prediction { get; set; }
+        //public float[] Score { get; set; }
+        [ColumnName("Probability")]
+        public float Probability { get; set; }
+
+        [ColumnName("Score")]
+        public float Score { get; set; }
+    }   
+}
---
+++ ML-Heart-Desease/Program.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using System;
+using System.Linq;
+
+namespace ML_Heart_Desease
+{    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ML Process started...");
+            #region 1. Create ML.NET context
+            var mlContext = new MLContext();
+            #endregion
+
+            #region 2. Load Data
+            IDataView data = mlContext.Data.LoadFromTextFile<HeartDesease>(@"..\..\..\Data\heart-disease.csv", ',',true);
+            #endregion
+            
+            #region Split data for training and testing
+            DataOperationsCatalog.TrainTestData dataSplit = mlContext.Data.TrainTestSplit(data, testFraction: 0.2);
+            IDataView trainingData = dataSplit.TrainSet;
+            IDataView testData = dataSplit.TestSet;
+
+            // Preview the data. 
+            var dataPreview = data.Preview();
+
+            Console.WriteLine($"There are {trainingData.Schema.Count} columns" + $" in the training dataset.");
+            Console.WriteLine($"There are {testData.Schema.Count} columns" +  $" in the test dataset.");
+            #endregion
+            #region 3. Get the column name of input features.
+
+            string[] featureColumnNames =
+                data.Schema
+                    .Select(column => column.Name)
+                    .Where(columnName => columnName != "Label").ToArray();
+            #endregion
+
+            #region 4. Define estimator with data pre-processing steps
+            IEstimator<ITransformer> dataPrepareEstimator = mlContext.Transforms.Concatenate("Features", featureColumnNames)
+                                                            .Append(mlContext.Transforms.NormalizeMinMax("Features"))                                                            
+                                                            .Append(mlContext.BinaryClassification.Trainers.AveragedPerceptron());
+            #endregion
+
+            #region Train the model
+
+            ITransformer model = dataPrepareEstimator.Fit(trainingData);      
+            // Use trained model to make inferences on test data
+            IDataView testDataPredictions = model.Transform(testData);
+            var calibratorEstimator = mlContext.BinaryClassification.Calibrators.Platt();
+            ITransformer calibratorTransformer = calibratorEstimator.Fit(testDataPredictions);
+            var finalData = calibratorTransformer.Transform(testDataPredictions).Preview();
+            PrintRowViewValues(finalData);
+            #endregion
+
+            #region Evaluate Model
+            CalibratedBinaryClassificationMetrics trainedModelMetrics = mlContext.BinaryClassification.Evaluate(calibratorTransformer.Transform(testDataPredictions));
+            // Print out accuracy metric
+            Console.WriteLine("Accuracy " + trainedModelMetrics.Accuracy);
+            #endregion
+        }
+
+        private static void PrintRowViewValues(Microsoft.ML.Data.DataDebuggerPreview data)
+        {
+            var firstRows = data.RowView.Take(5);
+
+            foreach (Microsoft.ML.Data.DataDebuggerPreview.RowInfo row in firstRows)
+            {
+                foreach (var kvPair in row.Values)
+                {
+                    if (kvPair.Key.Equals("Score") || kvPair.Key.Equals("Probability"))
+                        Console.Write($" {kvPair.Key} {kvPair.Value} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
